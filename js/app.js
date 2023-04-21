11523ab2b74883f05adb6e04cf1e225cc08787e1
dannyganza <kingdanny@Dannys-Air.hitronhub.home>
2023-04-21 10:42:21 -0600

    added more values

--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
 let usernames = ['Dad', 'mom', 'uncle', 'sister', 'brother'];
 let user_age = [12, 13, 15, 16, 17];
 
+let first_user_name = usernames[0];
+console.log (first_user_name);
+let last_user_name = usernames[4];
+console.log (last_user_name);
+let middle_user_age = user_age[2];
+console.log(middle_user_age);
